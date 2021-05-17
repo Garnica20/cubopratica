013c06b3c51683f14d5c81a7438b1ec5a3e8eb8d
Garnica20 <79761824+Garnica20@users.noreply.github.com>
2021-05-17 13:53:29 -0500

    Add files via upload

---
+++ cubopractica/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<meta charset=utf-8>
+		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+		<title>Beginning my Three.js Journey</title>
+        <script src="js/three.js"></script>
+	</head>
+	<body>
+    <script src="js/game.js"></script>
+	</body>
+</html>
